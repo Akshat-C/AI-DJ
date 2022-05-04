28ed5f5da2d2d5c463d018a18d3fa31ba31b8a3a
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-05-04 19:05:12 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+song = "";
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
 function setup()
 {
     canvas = createCanvas(510, 460);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 510, 460);
+}
+
+function play1()
+{
+    song.play();
 }
