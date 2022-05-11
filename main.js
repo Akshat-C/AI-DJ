4bfe1f659152aed1591e04411217feb7f495b38e
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-05-11 19:01:34 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+lwx = 0;
+lwy= 0;
+rwx = 0;
+rwy = 0;
 song = "";
 
 function preload()
@@ -11,6 +15,21 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+       console.log(results);
+       lwx = results[0].pose.leftWrist.x;
+       lwy = results[0].pose.leftWrist.y;
+       rwx = results[0].pose.rightWrist.x;
+       rwy = results[0].pose.rightWrist.y;
+       console.log("Left wrist x: "+lwx+" Left wrist y: "+lwy+" Right wrist x: "+rwx+" Right wrist y: "+rwy);
+    }
 }
 
 function draw()
@@ -20,5 +39,12 @@ function draw()
 
 function play1()
 {
+    song.setVolume(0.5);
+    song.rate(1);
     song.play();
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded")
 }
