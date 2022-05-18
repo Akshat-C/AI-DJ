cc8f34043cb17a9c8bde7ce70d84f185d85e24ce
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-05-18 19:18:06 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ lwy= 0;
 rwx = 0;
 rwy = 0;
 song = "";
+lw_score = "";
 
 function preload()
 {
@@ -11,7 +12,7 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(510, 460);
+    canvas = createCanvas(510, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -29,12 +30,26 @@ function gotPoses(results)
        rwx = results[0].pose.rightWrist.x;
        rwy = results[0].pose.rightWrist.y;
        console.log("Left wrist x: "+lwx+" Left wrist y: "+lwy+" Right wrist x: "+rwx+" Right wrist y: "+rwy);
+       lw_score = results[0].pose.keypoints[9].score;
     }
 }
 
 function draw()
 {
-    image(video, 0, 0, 510, 460);
+    image(video, 0, 0, 510, 500);
+
+    if (lw_score > 0.2)
+    {
+    fill("red");
+    stroke("red");
+    circle(lwx, lwy, 20);
+    num_lwy = Number(lwy);
+    whole_lwy = floor(num_lwy);
+    console.log(whole_lwy);
+    vol = whole_lwy/460;
+    song.setVolume(vol);
+    document.getElementById("volume").innerHTML = vol.toFixed(2);
+    }
 }
 
 function play1()
